ed7f7cc738bd4c3f92824537a2cf55c8ef3616ca
eraner <eraner21@gmail.com>
2017-07-12 15:07:45 +0300

    login page

--- Login/Login.php
+++ Login/Login.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link href="CSS/css.css" rel='stylesheet' type='text/css' />
+    <link href="../CSS/css.css" rel='stylesheet' type='text/css' />
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -13,7 +13,17 @@
     <div class="login-style">
         <h1>Login Page</h1>
         <form>
+            <input type="text" placeholder="Username" required/>
+            <input type="password" placeholder="Password" required/>
+            <div class="submit">
+                <input type="submit" value="Login" >
+            </div>
 
+            <div class="signup">
+                Not registered yet?
+                <a href="SignUp.php">Sign up</a>
+                for free now!
+            </div>
         </form>
     </div>
     <!--End login-style -->
