b48b80d954e7d3a1ce67c08f3246c2b5e08aa1e1
ohad <OhadCohen21@gmail.com>
2017-07-13 17:41:55 +0300

    Login passing user as parameter.

--- Login/Login.php
+++ Login/Login.php
@@ -1,12 +1,16 @@
 <?php
 require_once('..\DB\DatabaseHelper.php');
 
+session_start();
+
 if (isset($_POST["username"]) && isset($_POST["password"])){
     $db = new DatabaseHelper();
+    $username = $_POST["username"];
     $result = $db->ValidateUser($_POST["username"], $_POST["password"]);
 
     if ($result){
-        header("Location: ../Feed/FeedPage.php");
+        $_SESSION['loggedUser'] = $username;
+        header("Location: ../Feed/friends.php");
         return;
     } else {
         echo "Not Cool At All!!!";
