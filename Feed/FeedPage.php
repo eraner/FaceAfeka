0640e21bb85ab6748159d97942fdae984809da15
eraner <eraner21@gmail.com>
2017-07-13 13:18:30 +0300

    feed and start post

--- Feed/FeedPage.php
+++ Feed/FeedPage.php
@@ -16,17 +16,23 @@ $statusDetails = new StatusDetails($status, $imgSrc, $name);*/
 
 PrintHeadHTML();
 echo "<body>";
+
 AddTopNavigationBar();
+
+echo "<div style='padding-top: 30px'>";
 PrintStatusesListHTML($statusDetailsArr);
+echo "</div>";
 echo "</body>
         </html>";
 
 function AddTopNavigationBar(){
-    echo "<ul>
+    echo "<div class='navbar'>
+           <ul>
             <li><a class=\"active\" href='#'>FaceAfeka</a> </li>
-            <li><a href='#'>Friends</a> </li>
             <li><a href='#'>Profile</a> </li>
-           </ul>";
+            <li><a href='#'>Friends</a> </li>
+           </ul>
+           </div>";
 }
 
 function PrintHeadHTML(){
@@ -38,51 +44,23 @@ function PrintHeadHTML(){
                 content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">
             <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
             <title>Document</title>
-            <link href=\"../CSS/css.css\" rel=\"stylesheet\" type='text/css'>
+            <link href=\"../CSS/css.css\" rel=\"stylesheet\" type='text/css'/>
         </head>";
 }
 
 function PrintStatusesListHTML($statusDetailsArr){
     foreach ($statusDetailsArr as $statusDetails) {
-        /**Start div for status panel.*/
-        echo "<div class=\"panel panel-default\">";
-        /**Add user Name.*/
+        echo "<div>";
+        /**Add user Name*/
         echo "<h2>".$statusDetails->name."</h2>";
-        echo "<div class=\"panel-body\">";
         /**Add image if imgSrc isn't Blank*/
         if($statusDetails->imgSrc != ""){
-            echo "<p><img src=\"".$statusDetails->imgSrc."\" class=\"img-circle pull-right\"> <a href=\"#\">Picture here</a></p>";
+            echo "<p><img src=\"".$statusDetails->imgSrc."\"> <a href=\"#\">Picture here</a></p>";
         }
-        echo "<div class=\"clearfix\"></div>
-            <hr>";
+        echo "<hr>";
         /**Add status*/
         echo $statusDetails->status;
         echo "</div>";
     }
 }
-/*
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <a href="#" class="pull-right">View all</a>
-            <h4>Bootply Editor &amp; Code Library</h4>
-        </div>
-        <div class="panel-body">
-            <p><img src="//placehold.it/150x150" class="img-circle pull-right"> <a href="#">Picture here</a></p>
-            <div class="clearfix"></div>
-            <hr>
-            HERE is the STATUS from user.
-        </div>
-    </div>
-</body>
-</html>
-*/
+
---
+++ Feed/Post.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="../CSS/css.css" rel="stylesheet" type='text/css'/>
+    <title>Document</title>
+</head>
+<body>
+    <form action="/action_page.php">
+        <input type="file" name="pic" accept="image/*">
+    <input type="submit">
+</form>
+</body>
+</html>
+
