13f23474f69220712dd8cc8177e118a672d3e39b
eraner <eraner21@gmail.com>
2017-07-14 12:43:02 +0300

    Add new Post + get Posts of friends

--- Feed/UploadPost.php
+++ Feed/UploadPost.php
@@ -1,18 +1,31 @@
 <?php
 require_once ("..\Utils\HtmlHelper.php");
+require_once ("..\DB\DatabaseHelper.php");
 
 $status = $_POST['status'];
 $imgScr = $_POST['pic'];
 $privacy = $_POST['privacy'];
 $username = $_POST['loggedUser'];
 
+$db = new DatabaseHelper();
+$result = $db->InsertNewPost($status, $imgScr, $username, $privacy);
+if($result == true){
+    header("Location: FeedPage.php");
+    return;
+}
 
-PrintHeadHTML();
+echo "Error to post the status";
 
-echo $status."</br>";
-echo $imgScr."</br>";
-echo $privacy."</br>";
-echo $username."</br>";
-
-
-echo "</html>";
+//PrintHeadHTML();
+//
+//
+//
+//echo $status."</br>";
+//if($imgScr != "") {
+//    echo $imgScr . "</br>";
+//}
+//echo $privacy."</br>";
+//echo $username."</br>";
+//
+//
+//echo "</html>";
